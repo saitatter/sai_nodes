231396d86f98c0f67569c758e11abb5b5b9fd5e1
Voicu Andrei-Daniel <andrei.voicu133@gmail.com>
2026-09-08 23:42:04 +0300

    test(navigation): cover headless selection

---
+++ test/navigation_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sai_nodes/sai_nodes.dart';
+
+void main() {
+  test('navigates selection without a mounted Flutter widget tree', () {
+    final controller = NodeEditorController(
+      config: const NodeEditorConfig(
+        autoBuildGraph: false,
+        autoRunGraph: false,
+      ),
+    );
+    addTearDown(controller.dispose);
+
+    controller.registerNodePrototype(
+      NodePrototype(
+        idName: 'test.node',
+        displayName: (_) => 'Node',
+        description: (_) => 'Node',
+        onExecute: (_, _, _, _, _) async {},
+      ),
+    );
+
+    final origin = controller.addNode('test.node');
+    final next = controller.addNode(
+      'test.node',
+      offset: const Offset(120, 0),
+    );
+    controller.selectNodesById({origin.id});
+
+    expect(
+      controller.navigateSelection(NodeNavigationDirection.right),
+      next.id,
+    );
+    expect(controller.selectedNodeIds, {next.id});
+  });
+}
